851e760c3ff0acc1426697dd606d92b0c2558b4b
marcela <marcela@theofe.com>
2020-06-16 14:38:00 -0500

    Add decorator function for log in, and display the html template for search

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ from flask import Flask, session, redirect, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
+from decorator import login_required
+from HashPassword import HashPassword
 
 app = Flask(__name__)
 os.environ['DATABASE_URL'] = "postgres://fhtmmpjjvejjxs:a4fc62fd0a563452b14a6900423cd903199060e28207fb93fc06ccf9ae034723@ec2-3-222-30-53.compute-1.amazonaws.com:5432/d11g6lfpjmbrjn"
@@ -24,12 +25,19 @@ db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
+@login_required
 def index():
-    pass
+    """
+    Display the search box if the user is logged in
+    :return: website with the search boxes
+    """
+    return render_template("index.html")
 
 
-@app.route("/login", method=["POST"])
+'''
+@app.route("/login", methods=["POST"])
 def login():
     username = request.form.get("name")
     password = request.form.get("password")
     return render_template("login.html", username=username, password=password)
+'''

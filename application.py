9693ec11c0d09e8c365b1902ce3695cf396015eb
marcela <marcela@theofe.com>
2020-06-17 16:21:32 -0500

    Add function for login link

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 import os
-
 from flask import Flask, session, redirect, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from decorator import login_required
 from HashPassword import HashPassword
+
 
 app = Flask(__name__)
 os.environ['DATABASE_URL'] = "postgres://fhtmmpjjvejjxs:a4fc62fd0a563452b14a6900423cd903199060e28207fb93fc06ccf9ae034723@ec2-3-222-30-53.compute-1.amazonaws.com:5432/d11g6lfpjmbrjn"
@@ -30,14 +30,34 @@ def index():
     """
     Display the search box if the user is logged in
     :return: website with the search boxes
+    render_template("index.html")
     """
     return render_template("index.html")
 
 
-'''
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    Login route, where users can input their username and password
+    :return: login page
+    """
+    session.clear()
     username = request.form.get("name")
     password = request.form.get("password")
-    return render_template("login.html", username=username, password=password)
-'''
+    if request.method == "POST":
+        if username is None:
+            return render_template("error.html", message="Please provide username")
+        elif password is None:
+            return render_template("error.html", message="Please provide password")
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          {"username": username})
+        result = rows.fetchone()
+        hash_password = HashPassword()
+        if result is None or not hash_password.verify_password(stored_password=result[2],
+                                                               provided_password=password):
+            return render_template("error.html", message="Check username and password")
+        session["user_id"] = result[0]
+        session["user_name"] = result[1]
+        return redirect("/")
+    else:
+        return render_template("login.html")
